d63878b54a0f969a7f501e076bf6803d8980e420
Casadororo <51424037+Casadororo@users.noreply.github.com>
2023-09-10 20:38:48 -0300

    test files

---
+++ test/system/bubbles_test.rb
@@ -0,0 +1,47 @@
+require "application_system_test_case"
+
+class BubblesTest < ApplicationSystemTestCase
+  setup do
+    @bubble = bubbles(:one)
+  end
+
+  test "visiting the index" do
+    visit bubbles_url
+    assert_selector "h1", text: "Bubbles"
+  end
+
+  test "should create bubble" do
+    visit bubbles_url
+    click_on "New bubble"
+
+    fill_in "Category", with: @bubble.category_id
+    fill_in "Description", with: @bubble.description
+    fill_in "Name", with: @bubble.name
+    fill_in "Priority", with: @bubble.priority
+    click_on "Create Bubble"
+
+    assert_text "Bubble was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Bubble" do
+    visit bubble_url(@bubble)
+    click_on "Edit this bubble", match: :first
+
+    fill_in "Category", with: @bubble.category_id
+    fill_in "Description", with: @bubble.description
+    fill_in "Name", with: @bubble.name
+    fill_in "Priority", with: @bubble.priority
+    click_on "Update Bubble"
+
+    assert_text "Bubble was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Bubble" do
+    visit bubble_url(@bubble)
+    click_on "Destroy this bubble", match: :first
+
+    assert_text "Bubble was successfully destroyed"
+  end
+end
