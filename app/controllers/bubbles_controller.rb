8850bad2476d949779a46b10cff5bd1bf7aaac26
Casadororo <51424037+Casadororo@users.noreply.github.com>
2023-09-12 15:53:39 -0300

    delete buttons

--- app/controllers/bubbles_controller.rb
+++ app/controllers/bubbles_controller.rb
@@ -1,7 +1,6 @@
 class BubblesController < ApplicationController
   before_action :set_bubble, only: %i[ show edit update destroy ]
 
-
   # GET /bubbles or /bubbles.json
   def index
     @bubbles = Bubble.all
@@ -14,7 +13,7 @@ class BubblesController < ApplicationController
   # GET /bubbles/new
   def new
     @bubble = Bubble.new
-    @bubble.color =  "#f8f9fa"
+    @bubble.color = "#f8f9fa"
     @categories = Category.all
   end
 
@@ -42,12 +41,20 @@ class BubblesController < ApplicationController
   # PATCH/PUT /bubbles/1 or /bubbles/1.json
   def update
     respond_to do |format|
-      if @bubble.update(bubble_params)
-        format.html { redirect_to categories_url, notice: "Bubble was successfully updated." }
-        format.json { render :show, status: :ok, location: @bubble }
+      if params[:value] == "delete"
+        @bubble.destroy
+        respond_to do |format|
+          format.html { redirect_to categories_url, notice: "Bubble was successfully destroyed." }
+          format.json { head :no_content }
+        end
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @bubble.errors, status: :unprocessable_entity }
+        if @bubble.update(bubble_params)
+          format.html { redirect_to categories_url, notice: "Bubble was successfully updated." }
+          format.json { render :show, status: :ok, location: @bubble }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @bubble.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -63,13 +70,14 @@ class BubblesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_bubble
-      @bubble = Bubble.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def bubble_params
-      params.require(:bubble).permit(:name, :description, :priority, :category_id, :color)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_bubble
+    @bubble = Bubble.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def bubble_params
+    params.require(:bubble).permit(:name, :description, :priority, :category_id, :color)
+  end
 end
